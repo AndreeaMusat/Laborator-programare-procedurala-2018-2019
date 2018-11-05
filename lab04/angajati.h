3c053febbe024a054ac65ef28eca7c7ca9b83763
AndreeaMusat <andreea.a.musat@gmail.com>
2018-11-05 18:58:07 +0200

    complete lab 4 sol

--- lab04/angajati.h
+++ lab04/angajati.h
@@ -1,3 +1,19 @@
+void print_binary(char c) {
+  int bits[8];
+  memset(bits, 0, sizeof(bits));
+
+  for (unsigned i = 0; i <= 7; i++) {
+    if (c & (1 << i)) {
+      bits[i] = 1;
+    }
+  }
+
+  for (int i = 7; i >= 0; i--) {
+    printf("%d", bits[i]);
+  }
+  printf("\n");
+}
+
 typedef struct {
   // Standardul C nu specifica daca char este signed sau nu, asa ca vom
   // declara campurile ca unsigned pentru a ne asigura ca nu se pierde un bit
@@ -23,10 +39,26 @@ typedef struct {
 
 void pretty_print_angajat(Angajat a) {
   printf("Angajat %s cu varsta %d, ", a.nume, (int)a.varsta);
-  if (a.norma) printf("full-time ");
-  else printf("part-time ");
+  if (a.norma)
+    printf("full-time ");
+  else
+    printf("part-time ");
 
-  // TODO: print CNP
+  for (int i = 0; i <= 6; i++) {
+    // Daca vreti sa vedeti reprezentarea binara a CNP-ului, decomentati:
+    // print_binary(a.cnp[i]);
+
+    int digit0 = a.cnp[i] & (((char)1 << 4) - 1); // ultimii 4 biti din cnp[i]
+    int digit1 =
+        (a.cnp[i] >> 4) & (((char)1 << 4) - 1); // primii 4 biti din cnp[i]
+
+    // Primii 6 chars tin cate 2 digiti fiecare
+    if (i != 6)
+      printf("%d%d", digit0, digit1);
+    // Ultimul char tine doar ultimul digit din CNP
+    else
+      printf("%d\n", digit0);
+  }
 }
 
 Angajat read_angajat() {
@@ -39,8 +71,8 @@ Angajat read_angajat() {
   scanf("%u", &varsta);
 
   if (varsta >= 65) {
-  	printf("Varsta trebuie sa fie mai mica decat 65.\n");
-  	exit(-1);
+    printf("Varsta trebuie sa fie mai mica decat 65.\n");
+    exit(-1);
   }
 
   // Daca nu facem cast, varsta nu are tipul char, ci unsigned, asa cum
@@ -60,7 +92,24 @@ Angajat read_angajat() {
   // Numele il pot citi direct pentru ca nu e camp de biti.
   scanf("%s", a.nume);
 
-  // TODO: citire cnp
+  for (int i = 0; i < 13; i++) {
+    char digit;
+    scanf(" %c", &digit);
+
+    if (digit < '0' || digit > '9') {
+      printf("CNP-ul trebuie sa contina 13 cifre.\n");
+      exit(-1);
+    }
+
+    // Daca i este impar, trebuie sa setam primii 4 biti din a.cnp[i / 2]
+    if (i & 1) {
+      a.cnp[i / 2] |= (char)(digit - '0') << 4;
+    }
+    // Daca i este par, setam ultimii 4 biti din a.cnp[i / 2]
+    else {
+      a.cnp[i / 2] |= (char)(digit - '0');
+    }
+  }
 
   return a;
 }
